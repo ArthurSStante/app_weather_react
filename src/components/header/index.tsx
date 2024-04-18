711c6ecccf5ae7372493d96250bdc94339000dce
ArthurSStante <arthursaltori@gmail.com>
2024-04-18 13:02:47 -0300

    adicionando o header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <div className="text-3xl font-bold underline">Buscar Cidade</div>
+    </div>
+  );
+}
+
+export default Header;
