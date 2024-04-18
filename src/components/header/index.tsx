1cc2c6673f1cc8fbe63dd284ab2b6f450c2b9657
ArthurSStante <arthursaltori@gmail.com>
2024-04-18 15:05:45 -0300

    resolvendo o tailwind

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Search from "../Search";
 function Header() {
   return (
-    <div className="bg-gray-200 p-4">
-      <div className="text-3xl font-bold underline">Buscar Cidade</div>
+    <div>
+      {/* <div className="logo"></div> */}
+      <div className="bg-gray-200 flex items-center justify-center">
+        <div className="">Buscar Cidade</div>
+        <Search />
+      </div>
     </div>
   );
 }
